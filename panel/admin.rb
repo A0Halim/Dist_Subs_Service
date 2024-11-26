9bd4a4f0188adbb3c1d3164699bd572a39816bea
Ömer Faruk Derin <22060388@stu.omu.edu.tr>
2024-11-26 16:59:40 +0300

    Sürekli veri gönderimi ve alımı yapıldı

--- panel/admin.rb
+++ panel/admin.rb
@@ -4,71 +4,86 @@ require_relative 'capacity_pb'
 require 'socket'
 
 CONF = "dist_subs.conf"
-SERVER_PORT1 = 5002
+SERVER_PORT1 = 5001
+SERVER_PORT2 = 5002
+SERVER_PORT3 = 5003
+PYTHON_PORT = 5004
 
-class Server
-  def initialize(port)
-    @server = TCPServer.new(port)
-    puts "Admin server #{port} portunda calisiyor."
-  end
+def start(jserver1, jserver2, jserver3, pserver)
+  fault_tolerance_level = read_conf(CONF)
+  jconn1 = jserver1.accept
+  puts "Sunucu 1 ile bağlantı kuruldu"
+  jconn2 = jserver2.accept
+  puts "Sunucu 2 ile bağlantı kuruldu"
+  jconn3 = jserver3.accept
+  puts "Sunucu 3 ile bağlantı kuruldu"
+  pconn = pserver.accept
+  puts "Python sunucusu ile bağlantı kuruldu"
+  i = 1
 
-  def start
-    loop do
-    recv = @server.accept
-    handle_jserver(recv)
-    end
-  end
+  loop do
+    handle_jserver(jconn1, fault_tolerance_level, pconn, i)
+    handle_jserver(jconn2, fault_tolerance_level, pconn, i)
+    handle_jserver(jconn3, fault_tolerance_level, pconn, i)
 
-  def handle_jserver(recv)
-    puts "İstemci ile bağlantı kuruldu."
-
-    #dist_subs.conf dosyası okundu
-    value = 0
-    File.open(CONF, 'r') do |file|
-      file.each_line do |line|
-        arg = line.split('=', 2)
-        value = arg[1].to_i
-      end
-    end
-
-    begin
-      #Config dosyasından okunan veriyi java serverına yollama
-      response = Configuration.new(fault_tolerance_level: value, method_type: MethodType::STRT)
-      data = response.to_proto
-      recv.write([data.size].pack("N") + data)
-      puts "Gönderildi"
-
-      #Java serverından gelen başarılı başarısız message nesnesi
-      length = recv.read(4).unpack("N")[0]
-      rmessage = recv.read(length)
-      message = Message.decode(rmessage)
-      puts "Alindi: #{message.demand}, #{message.response}"
-
-      #Java serverından capacity nesnesi talebi
-      cpctyInquiry = Message.new(demand: Demand::CPCTY, response: Response::NULL)
-      cpctyData = cpctyInquiry.to_proto
-      recv.write([cpctyData.size].pack("N") + cpctyData)
-      puts "Gönderildi"
-
-      #Java serverından gelen capacity nesnesi
-      capacityLength = recv.read(4).unpack("N")[0]
-      receivedCapacity = recv.read(capacityLength)
-      receivedCpcty = Capacity.decode(receivedCapacity)
-      puts "Alindi: #{receivedCpcty.serverX_status}, #{receivedCpcty.timestamp}"
-
-      #Python'a capacity nesnesi gönderimi(Geçici olarak burda)
-      cpctyPlotter = Capacity.new(serverX_status: receivedCpcty.serverX_status, timestamp: receivedCpcty.timestamp)
-      plotterData = cpctyPlotter.to_proto
-      recv.write([plotterData.size].pack("N") + plotterData)
-      puts "Gönderildi"
-
-    rescue => e
-      puts "Bir hata oluştu: #{e.message}"
-    ensure
-      recv.close
-    end
+    i += 1
+    sleep(5)
   end
 end
 
-server = Server.new(SERVER_PORT1)
-server.start
+def read_conf(conf)
+  #dist_subs.conf dosyası okundu
+  value = 0
+  File.open(CONF, 'r') do |file|
+    file.each_line do |line|
+      value = line.split('=', 2).last.to_i
+    end
+  end
+  value
+end
+
+def python_handler(pconn, cpcty)
+  #Python'a capacity nesnesi gönderimi
+  cpctyPlotter = Capacity.new(serverX_status: cpcty.serverX_status, timestamp: cpcty.timestamp)
+  plotterData = cpctyPlotter.to_proto
+  pconn.write([plotterData.size].pack("N") + plotterData)
+  puts "Pythona capacity nesnesi gönderildi"
+end
+
+def handle_jserver(conn, fault_tolerance_level, pconn, i)
+
+  if(i == 1)
+  puts "İstemci ile bağlanti kuruldu."
+  #Config dosyasından okunan veriyi java serverına yollama
+  response = Configuration.new(fault_tolerance_level: fault_tolerance_level, method_type: MethodType::STRT)
+  data = response.to_proto
+  conn.write([data.size].pack("N") + data)
+  puts "Config gönderildi"
+  
+  #Java serverından gelen başarılı başarısız message nesnesi
+  length = conn.read(4).unpack("N")[0]
+  rmessage = conn.read(length)
+  message = Message.decode(rmessage)
+  puts "Başarili başarisiz message nesnesi alindi: #{message.demand}, #{message.response}"
+  end
+
+  #Java serverından capacity nesnesi talebi
+  cpctyInquiry = Message.new(demand: Demand::CPCTY, response: Response::NULL)
+  cpctyData = cpctyInquiry.to_proto
+  conn.write([cpctyData.size].pack("N") + cpctyData)
+  puts "Java serverindan capacity nesnesi istendi"
+
+  #Java serverından gelen capacity nesnesi
+  capacityLength = conn.read(4).unpack("N")[0]
+  receivedCapacity = conn.read(capacityLength)
+  receivedCpcty = Capacity.decode(receivedCapacity)
+  puts "Java serverinden gelen capacity nesnesi alindi: #{receivedCpcty.serverX_status}, #{receivedCpcty.timestamp} #{i}. tekrar"
+
+  python_handler(pconn, receivedCpcty)
+end
+
+  jserver1 = TCPServer.new(SERVER_PORT1)
+  jserver2 = TCPServer.new(SERVER_PORT2)
+  jserver3 = TCPServer.new(SERVER_PORT3)
+  pserver = TCPServer.new(PYTHON_PORT)
+  start(jserver1, jserver2, jserver3, pserver)

75801f067333605a559a3f5f0717091ca248e56b
Ömer Faruk Derin <22060388@stu.omu.edu.tr>
2024-12-10 18:38:35 +0300

    Iyileştirmeler yapıldı ve server durumundan client durumuna geçirildi

--- panel/admin.rb
+++ panel/admin.rb
@@ -4,86 +4,106 @@ require_relative 'capacity_pb'
 require 'socket'
 
 CONF = "dist_subs.conf"
-SERVER_PORT1 = 5001
-SERVER_PORT2 = 5002
-SERVER_PORT3 = 5003
-PYTHON_PORT = 5004
+SERVER_HOST = "localhost"
+JAVA_SERVER_PORTS = [5001, 5002, 5003]
+PYTHON_SERVER_PORT = 5004
 
-def start(jserver1, jserver2, jserver3, pserver)
-  fault_tolerance_level = read_conf(CONF)
-  jconn1 = jserver1.accept
-  puts "Sunucu 1 ile bağlantı kuruldu"
-  jconn2 = jserver2.accept
-  puts "Sunucu 2 ile bağlantı kuruldu"
-  jconn3 = jserver3.accept
-  puts "Sunucu 3 ile bağlantı kuruldu"
-  pconn = pserver.accept
-  puts "Python sunucusu ile bağlantı kuruldu"
-  i = 1
+def read_conf(conf)
+  # dist_subs.conf dosyasından fault_tolerance_level okuma işlemi
+  fault_tolerance_level = 0
+  File.open(conf, 'r') do |file|
+    file.each_line do |line|
+      fault_tolerance_level = line.split('=', 2).last.to_i
+    end
+  end
+  fault_tolerance_level
+end
 
+def connect_to_servers(ports)
+  connections = []
+
+  ports.each do |port|
+    begin
+      socket = TCPSocket.new(SERVER_HOST, port)
+      puts "Sunucu bağlantısı kuruldu. port: #{port}"
+      connections << socket
+    rescue Errno::ECONNREFUSED
+      puts "#{port} portu ile bağlantı kurulamadı."
+    end
+  end
+
+  connections
+end
+
+def send_config(socket, fault_tolerance_level)
+  # Configuration nesnesini Java sunucusuna gönder
+  config = Configuration.new(fault_tolerance_level: fault_tolerance_level, method_type: MethodType::STRT)
+  config_proto = config.to_proto
+  socket.write([config_proto.size].pack("N") + config_proto)
+  puts "Config gönderildi"
+
+  # Java sunucusundan yanıt mesajını al
+  response_length = socket.read(4).unpack("N")[0]
+  response_proto = socket.read(response_length)
+  response = Message.decode(response_proto)
+  puts "Sunucudan yanıt alındı: #{response.demand}, #{response.response}"
+
+  response
+end
+
+def request_capacity(socket)
+  # Java sunucusuna Capacity talebi gönder
+  request = Message.new(demand: Demand::CPCTY, response: Response::NULL)
+  request_proto = request.to_proto
+  socket.write([request_proto.size].pack("N") + request_proto)
+  puts "Capacity talebi gönderildi"
+
+  # Java sunucusundan Capacity yanıtını al
+  response_length = socket.read(4).unpack("N")[0]
+  response_proto = socket.read(response_length)
+  response = Capacity.decode(response_proto)
+  puts "Capacity alındı: #{response.serverX_status}, #{response.timestamp}"
+
+  response
+end
+
+def python_handler(socket, capacity)
+  # Capacity nesnesini Python sunucusuna gönder
+  capacity_proto = capacity.to_proto
+  socket.write([capacity_proto.size].pack("N") + capacity_proto)
+  puts "Python sunucusuna capacity nesnesi gönderildi: #{capacity.serverX_status}, #{capacity.timestamp}"
+end
+
+def server_connection_handler(java_connections, python_connection, fault_tolerance_level)
   loop do
-    handle_jserver(jconn1, fault_tolerance_level, pconn, i)
-    handle_jserver(jconn2, fault_tolerance_level, pconn, i)
-    handle_jserver(jconn3, fault_tolerance_level, pconn, i)
+    java_connections.each_with_index do |connection, index|
+      begin
+        puts "Java sunucu #{index + 1} çalışıyor."
+        response = send_config(connection, fault_tolerance_level)
 
-    i += 1
+        if response.response == "YEP"
+          capacity = request_capacity(connection)
+
+          if capacity
+            python_handler(python_connection, capacity)
+          else
+            puts "Java sunucu #{index + 1} kapasite bilgisi alınamadı."
+          end
+
+        else
+          puts "Java sunucusundan gerekli izinler alınamadı. Lütfen tekrar deneyin."
+        end
+
+        rescue IOError
+          puts "Java sunucu #{index + 1} ile bağlantı kesildi."
+      end
+    end
     sleep(5)
   end
 end
 
-def read_conf(conf)
-  #dist_subs.conf dosyası okundu
-  value = 0
-  File.open(CONF, 'r') do |file|
-    file.each_line do |line|
-      value = line.split('=', 2).last.to_i
-    end
-  end
-  value
-end
+fault_tolerance_level = read_conf(CONF)
+java_connections = connect_to_servers(JAVA_SERVER_PORTS)
+python_connection = connect_to_servers([PYTHON_SERVER_PORT]).first
 
-def python_handler(pconn, cpcty)
-  #Python'a capacity nesnesi gönderimi
-  cpctyPlotter = Capacity.new(serverX_status: cpcty.serverX_status, timestamp: cpcty.timestamp)
-  plotterData = cpctyPlotter.to_proto
-  pconn.write([plotterData.size].pack("N") + plotterData)
-  puts "Pythona capacity nesnesi gönderildi"
-end
-
-def handle_jserver(conn, fault_tolerance_level, pconn, i)
-
-  if(i == 1)
-  puts "İstemci ile bağlanti kuruldu."
-  #Config dosyasından okunan veriyi java serverına yollama
-  response = Configuration.new(fault_tolerance_level: fault_tolerance_level, method_type: MethodType::STRT)
-  data = response.to_proto
-  conn.write([data.size].pack("N") + data)
-  puts "Config gönderildi"
-  
-  #Java serverından gelen başarılı başarısız message nesnesi
-  length = conn.read(4).unpack("N")[0]
-  rmessage = conn.read(length)
-  message = Message.decode(rmessage)
-  puts "Başarili başarisiz message nesnesi alindi: #{message.demand}, #{message.response}"
-  end
-
-  #Java serverından capacity nesnesi talebi
-  cpctyInquiry = Message.new(demand: Demand::CPCTY, response: Response::NULL)
-  cpctyData = cpctyInquiry.to_proto
-  conn.write([cpctyData.size].pack("N") + cpctyData)
-  puts "Java serverindan capacity nesnesi istendi"
-
-  #Java serverından gelen capacity nesnesi
-  capacityLength = conn.read(4).unpack("N")[0]
-  receivedCapacity = conn.read(capacityLength)
-  receivedCpcty = Capacity.decode(receivedCapacity)
-  puts "Java serverinden gelen capacity nesnesi alindi: #{receivedCpcty.serverX_status}, #{receivedCpcty.timestamp} #{i}. tekrar"
-
-  python_handler(pconn, receivedCpcty)
-end
-
-  jserver1 = TCPServer.new(SERVER_PORT1)
-  jserver2 = TCPServer.new(SERVER_PORT2)
-  jserver3 = TCPServer.new(SERVER_PORT3)
-  pserver = TCPServer.new(PYTHON_PORT)
-  start(jserver1, jserver2, jserver3, pserver)
+server_connection_handler(java_connections, python_connection, fault_tolerance_level)
